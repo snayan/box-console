38dc2a03dc5c7f893824ea4fe6fefc46d5a8381b
三只小羊 <snayan@sina.com>
2018-10-02 16:27:35 +0800

    add examples

---
+++ examples/index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+const boxConsole = require('../dist');
+
+let msg = chalk(`New ${chalk.yellow('patch')} version of ${chalk.red('box-console')} available! ${chalk.red('0.1.0')} -> ${'0.1.1'}`);
+let tip = chalk(`Registry ${chalk.cyan('https://github.com/snayan/box-console')}`);
+let install = chalk(`Run ${chalk.green('npm i ' + 'box-console')} to update`);
+
+boxConsole([msg, tip, install]);
